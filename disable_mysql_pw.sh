4dcfd5d4bf03a3b007e8607031e28a4a45f1b805
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-14 11:06:33 -0600

    Update disable_mysql_pw.sh

--- disable_mysql_pw.sh
+++ disable_mysql_pw.sh
@@ -1,6 +1,6 @@
 sudo service mysql stop
 
-cat /etc/mysql/mysql.conf.d/mysql.cnf | awk 'BEGIN{fnd=0}{
+cat /etc/mysql/mysql.conf.d/mysqld.cnf | awk 'BEGIN{fnd=0}{
     if ($1 == "[mysqld]" && fnd == 0) {
         print "#skipstuffdone"
         print $0
@@ -15,6 +15,26 @@ cat /etc/mysql/mysql.conf.d/mysql.cnf | awk 'BEGIN{fnd=0}{
     else {
         print $0
     }
-}'
+}' > /tmp/mysqld.cnf
+sudo chmod a+r /tmp/mysqld.cnf
+sudo chown root:root /tmp/mysqld.cnf
+sudo cp /tmp/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
+sudo service mysql start
+
+mysql
+mysql> use mysql;
+mysql> flush privileges;
+mysql> update user set authentication_string=PASSWORD("YOUR-NEW-ROOT-PASSWORD") where User='root';
+mysql> UPDATE user SET plugin="mysql_native_password" WHERE User='root';
+mysql> quit
+
+sudo service mysql stop
+sudo vi /etc/mysql/mysql.conf.d/mysql.cnf
+# add following
+[mysqld]
+# For debugging and recovery only #
+#skip-grant-tables
+#skip-networking
+###################################
 sudo service mysql start
