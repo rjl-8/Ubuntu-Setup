1260c7b0eaaea4f1caf5c6014e632e04a61f235c
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-14 14:55:07 -0600

    Update reset_mysql_pw.sql

--- reset_mysql_pw.sql
+++ reset_mysql_pw.sql
@@ -6,6 +6,5 @@ PREPARE stmt FROM @query;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
---update user set authentication_string=PASSWORD("Passw0rd") where User='root';
 UPDATE user SET plugin="mysql_native_password" WHERE User='root';
 quit
