98c14f4c2d78939733b93c82b7bda3b5ef173660
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-14 10:34:06 -0600

    Update disable_mysql_pw.sh

--- disable_mysql_pw.sh
+++ disable_mysql_pw.sh
@@ -1,21 +1,14 @@
-cat /etc/ssh/sshd_config | awk '{
-    if ($1 == "#Port") {
-        print "Port 2200"
+cat /etc/mysql/mysql.conf.d/mysql.cnf | awk 'BEGIN{fnd=0}{
+    if ($1 == "[mysqld]" && fnd == 0) {
+        print "#skipstuffdone"
+        print $0
+        print "# For debugging and recovery only #"
+        print "skip-grant-tables"
+        print "skip-networking"
+        print "###################################"
     }
-    else if ($1 == "Port" && $2 != 2200) {
-        print "Port 2200"
-    }
-    else if ($1 == "#PasswordAuthentication") {
-        print "PasswordAuthentication no"
-    }
-    else if ($1 == "PasswordAuthentication" && $2 != "no") {
-        print "PasswordAuthentication no"
-    }
-    else if ($1 == "#PermitRootLogin") {
-        print "PermitRootLogin no"
-    }
-    else if ($1 == "PermitRootLogin" && $2 != "no") {
-        print "PermitRootLogin no"
+    else if ($1 == "#skipstuffdone") {
+        fnd = 1
     }
     else {
         print $0
